489d87abb6dee071bb8cb420bb1777cb0642f95b
rishabhmehra0107 <68009945+rishabhmehra0107@users.noreply.github.com>
2021-03-10 01:36:13 +0530

    Updated SQL integration in BankApp

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -1,12 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using Bank.Model;
-using Bank.Console.Data;
-using System.Linq;
-using SimpleInjector;
-using Bank.Contracts;
-using Bank.Services;
 
 namespace BankApp
 {
